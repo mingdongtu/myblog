78067ddf47f0db52a31005d964505bdff75143e7
mingdongtu <1518474913@qq.com>
2018-09-17 17:35:32 +0800

    引入ejs

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const app = express();
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/users')
 
-// app.get('/',function(req,res){
-//        res.send('Oh,my god! I am you ')
-// })
-// app.get('/users/:name',function(req,res){
-//        res.end('hello,' + req.params.name)
-// })
+app.set('')
 app.use('/',indexRouter);
 app.use('/users',userRouter);
 
